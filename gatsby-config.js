e6feef554126f2d4947f15ca3f3805876831b71d
Georgy Sorokin <georgywebdev@gmail.com>
2021-06-26 20:12:13 +0300

    manifest theme color

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,7 +34,7 @@ module.exports = {
         short_name: `GatsbyJS`,
         start_url: `/`,
         background_color: `#f7f0eb`,
-        theme_color: `#a2466c`,
+        theme_color: `#000000`,
         display: `standalone`,
         icon: `src/images/icon.png`,
       },
