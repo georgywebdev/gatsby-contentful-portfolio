626e159d04f61c09031e20e818c854f6e78a1be3
Georgy Sorokin <georgywebdev@gmail.com>
2021-06-26 20:29:06 +0300

    larger images

--- src/components/BlogBody.tsx
+++ src/components/BlogBody.tsx
@@ -1,19 +1,19 @@
-import React from "react"
-import Img from "gatsby-image"
+import React from "react";
+import Img from "gatsby-image";
 import {
   ContentfulRichTextGatsbyReference,
   renderRichText,
   RenderRichTextData,
-} from "gatsby-source-contentful/rich-text"
-import { BLOCKS, MARKS } from "@contentful/rich-text-types"
-import { Options } from "@contentful/rich-text-react-renderer"
+} from "gatsby-source-contentful/rich-text";
+import { BLOCKS, MARKS } from "@contentful/rich-text-types";
+import { Options } from "@contentful/rich-text-react-renderer";
 
-import { useContentfulImage } from "../hooks/useContentfulImage"
-import { Bold, Heading1, Text } from "./Markdown"
+import { useContentfulImage } from "../hooks/useContentfulImage";
+import { Bold, Heading1, Text } from "./Markdown";
 
 type BlogBodyProps = {
-  content: RenderRichTextData<ContentfulRichTextGatsbyReference>
-}
+  content: RenderRichTextData<ContentfulRichTextGatsbyReference>;
+};
 
 export const BlogBody = ({ content }: BlogBodyProps) => {
   const options: Options = {
@@ -24,20 +24,15 @@ export const BlogBody = ({ content }: BlogBodyProps) => {
       [BLOCKS.HEADING_1]: (node, children) => <Heading1>{children}</Heading1>,
       [BLOCKS.PARAGRAPH]: (node, children) => <Text>{children}</Text>,
       [BLOCKS.EMBEDDED_ASSET]: (node) => {
-        const asset = useContentfulImage(node.data.target.sys.id)
+        const asset = useContentfulImage(node.data.target.sys.id);
         if (asset) {
-          return (
-            <Img
-              fluid={asset.node.fluid}
-              style={{ maxWidth: "500px", margin: "5% auto" }}
-            />
-          )
+          return <Img fluid={asset.node.fluid} />;
         }
       },
     },
-  }
+  };
 
   return (
     <article className="blog-body">{renderRichText(content, options)}</article>
-  )
-}
+  );
+};
